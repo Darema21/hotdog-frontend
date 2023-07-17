3d93696f553c3a5bde8a44e3d1b39117e4387544
nastyam1994 <nastyam_94@bk.ru>
2023-07-18 00:36:05 +0800

    Making a card

--- pages/dogs/index.js
+++ pages/dogs/index.js
@@ -1,98 +1,121 @@
-// pages/dogs/index.js
-const utils = require('../../utils/util');
 const app = getApp()
+const utils = require('../../utils/util')
 
 Page({
+  data: {
+    pushList: [
 
-    /**
-     * Page initial data
-     */
-    data: {
+    ]
+  },
+  //事件处理函数
+  bindViewTap: function() {
+    
+  },
+  onLoad: function () {
+    setInterval(() => {
+      this.setData({
+        pushList: [
+        ]
+      })
+    }, 5000)
+  },
 
-    },
+  goToShow: function(e) {
+    const id = e.currentTarget.dataset.dogid
+    utils.goToShow(id)
+  },
 
-    goToShow(i) {
-        const id = e.currentTarget.dataset.dogid;
-        utils.goToShow(id);
-    },
+  /**
+   * Lifecycle function--Called when page load
+   */
+  onLoad(options) {
+    
+  },
 
-    /**
-     * Lifecycle function--Called when page load
-     */
-    onLoad(options) {
+  /**
+   * Lifecycle function--Called when page is initially rendered
+   */
+  onReady() {
 
-    },
+  },
 
-    /**
-     * Lifecycle function--Called when page is initially rendered
-     */
-    onReady() {
-
-    },
-
-    /**
-     * Lifecycle function--Called when page show
-     */
-    onShow() {
-      const page = this
+  /**
+   * Lifecycle function--Called when page show
+   */
+  onShow() {
+    const page = this
       
-      if (typeof this.getTabBar === 'function' &&
-      this.getTabBar()) {
-        this.getTabBar().setData({
-          selected: 0
-        })
-      }
-
-      wx.request({
-        url: `${app.globalData.baseUrl}dogs`,
-        method: 'GET',
-        header: getApp().globalData.header,
-        success(res) {
-            const dogs = res.data;
-            console.log(dogs)
-
-            //Update local data
-            page.setData({
-                dogs: dogs
-            });
-
-            wx.hideToast();
-        }
-      });
-    },
-
-    /**
-     * Lifecycle function--Called when page hide
-     */
-    onHide() {
-
-    },
-
-    /**
-     * Lifecycle function--Called when page unload
-     */
-    onUnload() {
-
-    },
-
-    /**
-     * Page event handler function--Called when user drop down
-     */
-    onPullDownRefresh() {
-
-    },
-
-    /**
-     * Called when page reach bottom
-     */
-    onReachBottom() {
-
-    },
-
-    /**
-     * Called when user click on the top right corner to share
-     */
-    onShareAppMessage() {
-
+    if (typeof this.getTabBar === 'function' &&
+    this.getTabBar()) {
+      this.getTabBar().setData({
+        selected: 0
+      })
     }
+
+    wx.request({
+      url: `${app.globalData.baseUrl}dogs`,
+      method: 'GET',
+      header: getApp().globalData.header,
+      success(res) {
+        const dogs = res.data;
+
+        // Update local data
+        const updatedDogs = dogs.map((dog) => {
+          return {
+            id: dog.id,
+            name: dog.name,
+            gender: dog.gender,
+            image_url: dog.image_urls ? dog.image_urls[0] : '', // Get the first image URL
+            neutered: dog.neutered,
+            vaccinated: dog.vaccinated,
+            // Add more properties: breed and owner
+          };
+        });
+  
+        page.setData({
+          pushList: updatedDogs, // Set the updated dogs array to pushList
+        }, () => {
+          console.log("Push List", page.data.pushList)
+        });
+        
+        
+        wx.hideToast();
+      },
+    });
+  },
+
+  /**
+   * Lifecycle function--Called when page hide
+   */
+  onHide() {
+
+  },
+
+  /**
+   * Lifecycle function--Called when page unload
+   */
+  onUnload() {
+
+  },
+
+  /**
+   * Page event handler function--Called when user drop down
+   */
+  onPullDownRefresh() {
+
+  },
+
+  /**
+   * Called when page reach bottom
+   */
+  onReachBottom() {
+
+  },
+
+  /**
+   * Called when user click on the top right corner to share
+   */
+  onShareAppMessage() {
+
+  }
 })
--- utils/util.js
+++ utils/util.js
@@ -46,5 +46,6 @@ function goToBreed(e) {
 
 module.exports = {
   formatTime,
-  goToBreed
+  goToBreed,
+  goToShow
 }

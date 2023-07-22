cf44b559cb770b1889d7e0785f53c96ff1829b07
(tara hodreal) tara@castrogrimnitz.com <tara@castrogrimnitz.com>
2023-07-22 16:08:46 +0800

    pages-matches-show

--- pages/matches/show.js
+++ pages/matches/show.js
@@ -1,20 +1,53 @@
 // pages/matches/show.js
+const app = getApp()
 Page({
 
     /**
      * Page initial data
      */
     data: {
-
+      owner: "Owner",
+      message: "Message"
     },
-
+    formSubmit (e) {
+      let owner = e.detail.value.owner;
+      let message = e.detail.value.message;
+      let story = {
+        owner: owner,
+        message: message
+      }
+      wx.request({
+        url: `http://localhost:3000/matches`,
+        method: 'POST',
+        data: story,
+        success() {
+          // redirect to index page when done
+          wx.redirectTo({
+            url: '/pages/matches/show'
+          });
+        }
+      });
+    },
     /**
      * Lifecycle function--Called when page load
      */
     onLoad(options) {
-
+      let page = this;
+      // Get api data
+      wx.request({
+        url: "http://localhost:3000/matches",
+        method: 'GET',
+        success(res) {
+          const messages = res.data.messages;
+          console.log(res)
+          // Update local data
+          page.setData({
+            messages: messages
+          });
+          wx.hideToast();
+        }
+      });
     },
-
     /**
      * Lifecycle function--Called when page is initially rendered
      */
@@ -25,16 +58,16 @@ Page({
     /**
      * Lifecycle function--Called when page show
      */
-    onShow() {
-        const page = this
+    // onShow() {
+    //     const page = this
       
-        if (typeof this.getTabBar === 'function' &&
-        this.getTabBar()) {
-          this.getTabBar().setData({
-            selected: 2
-          })
-        }
-    },
+    //     if (typeof this.getTabBar === 'function' &&
+    //     this.getTabBar()) {
+    //       this.getTabBar().setData({
+    //         selected: 2
+    //       })
+    //     }
+    // },
 
     /**
      * Lifecycle function--Called when page hide

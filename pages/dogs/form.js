069289a1c4e1cc246cd03ffd5dff96759648ddb0
Darema Dzhold <darema.dzhold@outlook.com>
2023-07-29 18:57:42 +0800

    successfully get and post owner id

--- pages/dogs/form.js
+++ pages/dogs/form.js
@@ -7,21 +7,16 @@ Page({
     data: {
       name: "",
       gender: ["male", "female"],
-      // age: '00', 
       age: '',
       breed: ["Beagle", "Bulldog", "Chihuahua", "Corgi", "French Bulldog", "German Shepherd", "Golden Retriever", "Husky", "Labrador", "Parson Russell Terrier", "Pug", "Poodle (Toy)", "Other"],
       breed_value: "",
       gender_value: "",
       bio: "",
-      // neutered: ["neutered", "not neutred"],
-      // vaccinated: ["vaccinated", "not vaccinated"],
       neutered: false,
       vaccinated: false,
       address: "",
-      owner: "",
       formData: {},
       src: ""
-      // src: [],
     },
 
     upload(id) {
@@ -84,7 +79,7 @@ Page({
       const page = this
       console.log("form onShow")
       let { formData } = page.data
-
+      console.log('success onShow, page.data', page.data)
       // comment out for now
       // if (this.data.resetData) this.resetData()
       if(wx.getStorageSync('new')) {
@@ -140,12 +135,13 @@ Page({
     // neutered switch
     onSwitchNeutered(e) {
       console.log('e.detail.value from SwitchNeutered', e.detail.value)
-      this.onChange('switchNeutered', e)
+      this.onChange('neutered', e)
+      // this.onChange('switchNeutered', e)
     },
     // vaccinated switch
     onSwitchVaccinated(e) {
       console.log('e.detail.value from SwitchVaccinated', e.detail.value)
-      this.onChange('switchVaccinated', e)
+      this.onChange('vaccinated', e)
     },
     // this method sets data when input Neutered, Vaccinated
     onChange(field, e) {
@@ -272,10 +268,8 @@ Page({
       console.log(dog)
       console.log("---------------------------")
 
-      console.log('from save tbn, Update: dog ->', dog)
+      console.log('from save btn, page.data ->', page.data)
       page.setData({dog})
-      console.log('this data to send ->', page.data.dog)
-
       // const event = e.detail.value;
       // event.owner_id = 1;
   
@@ -285,7 +279,10 @@ Page({
           header: app.globalData.header,
           url: `${app.globalData.baseUrl}dogs/${page.data.editedId}`,
           method: 'PUT',
-          data: { dog: dog },
+          data: { 
+            dog: dog, 
+            owner_id: app.globalData.owner.id
+          },
           success(res) {
             console.log('update success? ', res)
             page.upload(page.data.editedId)
@@ -297,14 +294,21 @@ Page({
         });
       } else {
         // if dog id doesn't exist, create new form
-        console.log('Create: dog ', dog)
+        console.log('Create: new dog ', dog)
+        console.log('this data to send ->', page.data.dog)
+        console.log('this owner_id to send ->',  app.globalData.owner.id)
         wx.request({
           header: app.globalData.header,
           url: `${app.globalData.baseUrl}dogs/`,
           method: 'POST',
-          data: { dog: dog },
+          data: { 
+            dog: {
+              ...dog,
+              owner_id: app.globalData.owner.id
+            },       
+          },
           success(res) {
-            console.log('create success?', res);
+            console.log('create success?', res)
             if (res.statusCode === 422) {
               wx.showModal({
                 title: 'Sorry, please try again',
@@ -321,7 +325,14 @@ Page({
             } else {
               wx.showToast({
                 title: 'Dog Created!',
-                duration: 2000
+                duration: 2000,
+                success(resolve) {
+                  setTimeout(() => {
+                    wx.navigateTo({
+                      url: `/pages/dogs/show?=${page.data.id}`
+                    })
+                  })
+                }
               })
               // call the upload
               const id = res.data.id
@@ -345,9 +356,9 @@ Page({
         header: app.globalData.header,
         name: 'image',
         success (res) {
-          // wx.navigateTo({
-          //   url: `/pages/dogs/index`
-          // })
+          wx.navigateTo({
+            url: `/pages/dogs/index`
+          })
           page.setData({resetForm: true})
           console.log('res from upload', res)
         }
